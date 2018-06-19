e44f382a84fb2d8a07f7d1a3180d6e79f6274058
Sean Urgel <seantheurgel@gmail.com>
2018-06-19 13:49:34 +0800

    Finished routing feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import Navbar from 'components/Navigation/Navbar/Navbar';
+import Home from 'containers/Home/Home';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/messages' render={() => {
+            return <h1>Messages</h1>
+          }}/>
+        </Switch>
       </div>
     );
   }
