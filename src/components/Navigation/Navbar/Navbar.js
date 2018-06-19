0f6122d2edeeae3883a8d62302e26d142cf2c975
Sean Urgel <seantheurgel@gmail.com>
2018-06-19 13:03:05 +0800

    first commit

---
+++ src/components/Navigation/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const navbar = () => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <span className="navbar-brand mb-0 h1">Navbar</span>
+    </nav>
+  )
+}
+
+export default navbar;
+
