0d31848661b780b39986194bd25d6da4b155b8ac
Sean Urgel <seantheurgel@gmail.com>
2018-06-19 13:56:45 +0800

    Created ProfileCard folder

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Home</h1>
+      <div className="container">
+        
       </div>
     )
   }
