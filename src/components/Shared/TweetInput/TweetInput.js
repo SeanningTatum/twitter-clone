bb3db39c969b7d731ff280a4f8d696b94983017c
Sean Urgel <seantheurgel@gmail.com>
2018-06-19 16:15:56 +0800

    finished tweetInput

--- src/components/Shared/TweetInput/TweetInput.js
+++ src/components/Shared/TweetInput/TweetInput.js
@@ -2,13 +2,18 @@ import React from 'react'
 
 const tweetInput = () => {
   return (
-    <div className="row">
-      <div className="col-md-2">
-        img
-      </div>
-      <div className="col-md-10">
-        <div className="form-group">
-          <textarea className="form-control" placeholder="What's Happening?" rows="3"></textarea>
+    <div className="card bg-light p-2">
+      <div className="row mt-2">
+        <div className="col-md-2">
+          img
+        </div>
+        <div className="col-md-10">
+          <div className="form-group">
+            <textarea className="form-control" placeholder="What's Happening?" rows="3"></textarea>
+          </div>
+          <div className="text-right mr-2">
+            <button className="btn btn-success">Tweet</button>
+          </div>
         </div>
       </div>
     </div>
