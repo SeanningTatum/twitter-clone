30a84397042e7463847fdae43ac727a39383b05a
Sean Urgel <seantheurgel@gmail.com>
2018-06-19 14:20:46 +0800

    Added listgroup content

--- src/components/Shared/TweetFeed/TweetFeed.js
+++ src/components/Shared/TweetFeed/TweetFeed.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const tweetFeed = () => {
   return (
-    <div>
-      
+    <div classNameName="list-group">
+      <a className="list-group-item list-group-item-action flex-column align-items-start active">
+        <div className="d-flex w-100 justify-content-between">
+          <h5 className="mb-1">List group item heading</h5>
+          <small>3 days ago</small>
+        </div>
+        <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+        <small>Donec id elit non mi porta.</small>
+      </a>
     </div>
   )
 }
