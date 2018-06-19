5a6e82876c8e9f99ee27e4527fe5e5a0085c09cb
Sean Urgel <seantheurgel@gmail.com>
2018-06-19 21:50:54 +0800

    created  fetchTweets to make reducer simplier

--- src/store/reducers/tweets.js
+++ src/store/reducers/tweets.js
@@ -10,9 +10,29 @@ const initState = {
    addTweetInput: true
 }
 
+const fetchTweets = (state) => {
+   return {
+      ...state, 
+      tweets: [
+         {
+            name: "Kyra Lei",
+            handlerName: "@leideroda",
+            message: "Hi, I like art",
+            date: '6/30/1998'
+         },
+         {
+            name: "Sean Urgel",
+            handlerName: "@mrseanurgel",
+            message: "Making a project to impress employers hehe please accept me",
+            date: '10/10/1998'
+         }
+      ]
+   }
+}
+
 const reducer = (state = initState, action) => {
    switch (action.type) {
-      case "FETCH_TWEETS": return {...state, tweets: [1,2]}
+      case "FETCH_TWEETS": return fetchTweets(state);
       default: return state;
    }
 }
