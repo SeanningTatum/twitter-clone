d487e072b30ea5cd67da53de89d3820791746cad
Sean Urgel <seantheurgel@gmail.com>
2018-06-19 14:11:11 +0800

    Added content and layout to Home and Profile Card

--- src/components/Home/ProfileCard/ProfileCard.js
+++ src/components/Home/ProfileCard/ProfileCard.js
@@ -2,11 +2,23 @@ import React from 'react';
 
 const profileCard = () => {
   return (
-    <div className="card" style="width: 18rem;">
+    <div className="card" style={{width: '18rem'}}>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a className="btn btn-primary">Go somewhere</a>
+        <h5 className="card-title">User name</h5>
+        <div className="row">
+          <div className="col-4">
+            <p className="small">Tweets</p>
+            <p>256</p>
+          </div>
+          <div className="col-4">
+            <p className="small">Follwers</p>
+            <p>256</p>
+          </div>
+          <div className="col-4">
+            <p className="small">Following</p>
+            <p>256</p>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import ProfileCard from 'components/Home/ProfileCard/ProfileCard';
 
 export class Home extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-md-4">
+            <ProfileCard />
+          </div>
+          <div className="col-md-8">
+          
+          </div>
+        </div>
         
       </div>
     )
