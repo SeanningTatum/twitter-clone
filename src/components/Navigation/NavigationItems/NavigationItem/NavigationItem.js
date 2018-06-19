96415806461e25e499c884b09d1dcc5671bb1d1f
Sean Urgel <seantheurgel@gmail.com>
2018-06-19 13:19:02 +0800

    created navigation item and outsourced files

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const navigationItem = () => {
+  return (
+    <li className="nav-item">
+      <a className="nav-link">Messages</a>
+    </li>
+  )
+}
+
+export default navigationItem;
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,19 +1,12 @@
 import React from 'react';
+import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => {
    return (
       <React.Fragment>
          <ul className="nav">
-          <li className="nav-item">
-            <a className="nav-link">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link">Notifications</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link">Messages</a>
-          </li>
-        </ul>
+            <NavigationItem></NavigationItem>
+         </ul>
       </React.Fragment>
    )
 }
