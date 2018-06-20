e253d2b01671696a5a192dc63a12d1d888c45003
Sean Urgel <seantheurgel@gmail.com>
2018-06-20 21:51:34 +0800

    created fetchTweets code to get data from server

--- src/store/actions/tweets.js
+++ src/store/actions/tweets.js
@@ -1,16 +1,29 @@
 import * as actionTypes from './actionTypes';
 
-export const fetchTweets = () => {
+
+export const getTweets = () => {
    return {
       type: actionTypes.FETCH_TWEETS
    }
 }
 
 // Add tweet to parameters
-export const postTweet = (tweet) => {
-   
+export const postTweet = (tweet) => {  
    return {
       type: actionTypes.POST_TWEET,
       tweet
    }
+}
+
+export const fetchTweets = () => {
+   return dispatch => {
+      fetch("http://localhost:3000/")
+         .then(response => response.json())
+         .then(data => {
+            console.log(data.response);
+            dispatch(getTweets())
+         }).catch(err => {
+            console.error(err);
+         });
+   } 
 }
