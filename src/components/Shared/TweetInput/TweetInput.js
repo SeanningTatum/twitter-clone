9bcded815507c10fa40ea4fb2e60dbcac9b18d14
Sean Urgel <seantheurgel@gmail.com>
2018-06-20 14:47:19 +0800

    made forms in the react way

--- src/components/Shared/TweetInput/TweetInput.js
+++ src/components/Shared/TweetInput/TweetInput.js
@@ -1,29 +1,51 @@
-import React from 'react'
-import Proptypes from 'prop-types';
+import React, { Component } from 'react'
+import Proptypes from 'prop-types'
 
-const tweetInput = (props) => {
-  return (
-    <div className="card bg-light p-2">
-      <div className="row mt-2">
-        <div className="col-md-2">
-          img
-        </div>
-        <div className="col-md-10">
-          <div className="form-group">
-            <textarea className="form-control" placeholder="What's Happening?" rows="3"></textarea>
+export default class TweetInput extends Component {
+  static propTypes = {
+    clicked: Proptypes.func.isRequired
+  }
+
+  state = {
+    tweet: '',
+    rowSize: 1
+  }
+
+  changeTextAreaSize = (size) => {
+    this.setState({rowSize: size});
+  }
+
+  handleChange = (event) => {
+    this.setState({tweet: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className="card bg-light p-2">
+        <div className="row mt-2">
+          <div className="col-md-2">
+            img
           </div>
-          <div className="text-right mr-2">
-            <button className="btn btn-success" onClick={props.clicked}>Tweet</button>
+          <div className="col-md-10">
+            <div className="form-group">
+              <textarea 
+                className="form-control" 
+                placeholder="What's Happening?" 
+                value={this.state.tweet}
+                onChange={(event) => this.handleChange(event)} 
+                onBlur={() => this.changeTextAreaSize(1)}
+                onFocus={() => this.changeTextAreaSize(3)}
+                rows={this.state.rowSize}></textarea>
+            </div>
+            <div className="text-right mr-2">
+              <button className="btn btn-success" onClick={this.props.clicked}>Tweet</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
-}
-
-tweetInput.propTypes = {
-  clicked: Proptypes.func.isRequired
+    )
+  }
 }
 
 
-export default tweetInput
+ 
