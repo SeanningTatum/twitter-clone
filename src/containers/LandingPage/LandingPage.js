3fcd1877b91389f4f2d52f156417978d0da748aa
Sean Urgel <seantheurgel@gmail.com>
2018-06-22 14:54:38 +0800

    added buttons for logging in and logging out

---
+++ src/containers/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <button className="btn btn-primary">Log in</button>
+        <button className="btn btn-primary">Sign up</button>
+      </div>
+    )
+  }
+}
+
+export default LandingPage
