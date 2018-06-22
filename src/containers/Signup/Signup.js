cfd7919542cfff064e12ce00c13a37b3ec9e6a42
Sean Urgel <seantheurgel@gmail.com>
2018-06-22 16:11:21 +0800

    added onSubmit for testing purposes

--- src/containers/Signup/Signup.js
+++ src/containers/Signup/Signup.js
@@ -13,7 +13,9 @@ export class Signup extends Component {
     return (
       <div className="container mt-5">
         <form className="col-md-7 mx-auto">
-          <FormControl type="input" placeholder={"Email"} label={"Username"}/>
+          <FormControl type="input" placeholder={"Email"} label={"Email"}/>
+          <FormControl type="input" placeholder={"Username"} label={"Username"}/>
+          <FormControl type="input" placeholder={"Full Name"} label={"Full Name"}/>
           <FormControl type="password" placeholder={"Password"} label={"Password"}/>
           <div className="text-right">
             <button className="btn btn-primary">Submit</button>
